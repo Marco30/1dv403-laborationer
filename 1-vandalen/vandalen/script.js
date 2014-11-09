50826eb1560809de288f99d20370202e19bfcfe3
Marco Villegas <mv222fp@student.lnu.se>
2014-11-09 20:12:39 +0000

    lab 1.4 nästan klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -9,14 +9,33 @@ var makePerson = function(persArr){
 	
 	var nameage = persArr.slice();
 	
+	var name = "";
+	var age =[];
+	
+nameage.sort(function(a, b)
+{
+ return a.age-b.age
+})
+
+for(var i = 0; i < nameage.length; i+=1) 
+{
+    name += " " + nameage[i].name;
+    age [i]= nameage[i].age;
+
+}
+var minAge = Math.min.apply( Math, age); 
+var maxAge = Math.max.apply( Math, age);
+var avarageAge = age[(age.length - 1) / 2];
+ 
+
+console.log( "minAge: " + minAge + "maxAge: " + maxAge + " averageAge: " + avarageAge + " names: " + name);
 
 
-alert(JSON.stringify(nameage));
 
 };
 
 
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = [{name: "Mats Loock", age: 46}, {name: "Johan Leitet", age: 36}, {name: "John Häggerud", age: 37}];
 
 var result = makePerson(data);
 
