9bbf2ed2629d24e29c6997c493785ab5d1288abc
Marco Villegas <mv222fp@student.lnu.se>
2014-11-09 18:05:42 +0000

    lab 1.4 jobbar på den

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,23 @@
 "use strict";
 
+
+
 var makePerson = function(persArr){
 
 
 	// Din kod här...
+	
+	var nameage = persArr.slice();
+	
 
-}
 
+alert(JSON.stringify(nameage));
+
+};
+
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result); 
