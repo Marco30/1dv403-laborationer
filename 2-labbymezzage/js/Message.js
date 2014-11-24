fc8716a20d617dbc67843a80173a0ae1cf392632
Marco Villegas <mv222fp@student.lnu.se>
2014-11-24 23:24:41 +0000

    moment 1-2

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -1,25 +1,36 @@
 "use strict";
 
-function Message(message, date) {
+function Message(message, date) 
+{
     
-    this.getText = function() {
+    this.getText = function() 
+    {
         return message;
     };
     
-    this.setText = function(_text) {
+    this.setText = function(_text) 
+    {
         message = _text;
     };
     
-    this.getDate = function() {
+    this.getDate = function() 
+    {
         return date;
     };
         
-    this.setDate = function(_date) {
+    this.setDate = function(_date) 
+    {
         date = _date;
     };
 }
 
-Message.prototype.toString = function() {
+Message.prototype.toString = function() 
+{
     return this.getText()+" ("+this.getDate()+")";
 };
 
+ var mess = new Message("marco", new Date());
+ alert(mess);
+ alert(mess.getText());
+ mess.setText("enn annan text");
+ alert(mess);
