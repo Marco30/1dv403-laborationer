1817bd36a1e0f99fe061001612282d49c42aa794
Marco Villegas <mv222fp@student.lnu.se>
2014-11-27 00:41:18 +0000

    klar med klock ikonen, ska jobba på tabort ikonen

--- 2-labbymezzage/js/MezzageBoard.js
+++ 2-labbymezzage/js/MezzageBoard.js
@@ -42,8 +42,11 @@
             antal.innerHTML = number;
         },
         
-        textin : function(tex) //funktionen skriver in texterna på HTML sidan så att det kan presenteras 
+        textin : function(tex) //funktionen skriver in texterna och ikoner på HTML sidan så att det kan presenteras 
         {
+            
+             //---------------------Text--------------------------------
+             
              var text1 = document.createElement("div");//Variabeln text1 får HTML koden <div>
              
             var p = document.createElement("p"); //Variabeln p får HTML koden <p>
@@ -57,7 +60,7 @@
             p.className = "textinput";// <p> till delas klassa namnet newmessage <p class="textinput"
             
         
-          //-----------------------------------------------------
+          //---------------------Tid--------------------------------
           
           
             var tid = document.createElement("p");// Variabeln tid får HTML koden <p>
@@ -67,8 +70,41 @@
             tid.className = "tid";// <p> till delas klassa namnet tid <p class="tid"
             
              text1.appendChild(tid);// <div > får <p> med tid
+
         
-        document.getElementById("textonpage").appendChild(text1);// Lägger in den nya HTML koden i <div id = "textonpage"> så att det syns på sidan
+        
+            //-------------------ikon div--------------------------------
+        
+         
+            var ikon = document.createElement("div");// Variabeln Ikon1tid får HTML koden <span>
+            
+            ikon.className = "ikon";// <div> till delas klassa namnet ikon <div class="ikon">
+            
+             //-------------------tid ikon--------------------------------
+            
+            var tidlenk = document.createElement("a");// Variabeln tidikon får HTML koden <a>
+            
+            var bildkod = document.createElement("img");// variabeln bildkod får HTML koden <img>
+            
+            bildkod.className = "tidikon";// <img> till delas klassa namnet tidikon <div class="tidikon">
+            
+            bildkod.setAttribute("src", "pics/tid.png");// <img> Egenskapen som länkar till bild och själva sökvägen till bilden 
+            
+            bildkod.onclick = function() // visar tiden i en ruta när man klickar på tid ikonen 
+            {
+                alert("Inlägget skapades " + MezzageBoard.messages[tex].getDatetext().toLocaleDateString() +
+                " klockan " + MezzageBoard.messages[tex].getDatetext().toLocaleTimeString());
+            }
+            
+           ikon.appendChild(tidlenk);//  Variabeln ikon får HTML koden <a>
+           
+           tidlenk.appendChild(bildkod);//  Variabeln tidlenk får HTML koden <img>
+          
+           text1.appendChild(ikon);// <div > får <a> med tid
+           
+           //-------------------skriver ut--------------------------------
+           
+           document.getElementById("textonpage").appendChild(text1);// Lägger in den nya HTML koden i <div id = "textonpage"> så att det syns på sidan
         
         }
     };
